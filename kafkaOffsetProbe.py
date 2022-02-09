a6afc177b3070dccec236482f26eeeeca733a24e
gmadip <35444080+gmadip@users.noreply.github.com>
2022-02-09 11:47:53 -0800

    Final version. Removing Helper file

--- kafkaOffsetProbe.py
+++ kafkaOffsetProbe.py
@@ -7,10 +7,10 @@ Created on 6 Nov 2017
 import os
 from flask import Flask
 from prometheus_client import Gauge, CollectorRegistry, generate_latest
-import helper
 import logging
-import app_config
 import json
+import asyncio
+import pykafka
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,11 +21,15 @@ app = Flask(__name__)
 registry = CollectorRegistry()
 producerOffsetTracker = Gauge('kafka_offset', 'the latest available offsets', ['service', 'topic', 'partition'], registry=registry)
 consumerGroupOffsetTracker = Gauge('cg_kafka_offset', 'the consumer group offsets', ['service', 'topic', 'partition', 'consumergroup'], registry=registry)
-
+cgOffsetsInfo_list = []
+offset_info = {}
 
 def readEnvConfig(env_var_name, default=""):
     env_var_val = os.environ.get(env_var_name, default)
     return env_var_val.strip()
+
+kafka_hosts = readEnvConfig("KAFKAHOSTS")
+client = pykafka.KafkaClient(hosts=kafka_hosts)
 
 @app.route('/ping')
 def ping():
@@ -35,9 +39,7 @@ def ping():
 @app.route('/internal/metrics')
 def getKafkaOffset():
     # fetch the latest available offsets from kafka and return the metrics
-    kafka_hosts = readEnvConfig("KAFKAHOSTS")
-    kafka_helper = helper.KafkaHelper(kafka_hosts)
-    topic_offset_info = kafka_helper.getLatestOffset()
+    topic_offset_info = getLatestOffset()
     #LOGGER.info(topic_offset_info)
 
     if topic_offset_info:
@@ -46,17 +48,105 @@ def getKafkaOffset():
                 producerOffsetTracker.labels('broker', offsetInfo['topic'], offsetInfo['partition']).set(offsetInfo['offset'])
 
     # check consumer group offsets
-    consumerGroupListJsonStr=readEnvConfig("CONSUMERGROUPS")
+    consumerGroupListJsonStr = readEnvConfig("CONSUMERGROUPS")
     if consumerGroupListJsonStr:
         consumerGroupsToCheck = json.loads(consumerGroupListJsonStr)
+
         # go through the list of (consumergroup: topic)
-        for topicName, cgNames in consumerGroupsToCheck.items():
-            for cgName in cgNames:
-                cgOffsetsInfo = kafka_helper.getConsumerGroupOffsets(topicName, cgName)
-                for partition_id, offset in cgOffsetsInfo.items():
-                    consumerGroupOffsetTracker.labels('broker', topicName, partition_id, cgName).set(offset)
+        cg = createCGAsync(consumerGroupsToCheck)
 
     return generate_latest(registry)
+
+def createCGAsync(consumerGroupsToCheck):
+    tasks_cg = []
+    for topicName, cgNames in consumerGroupsToCheck.items():
+        for cgName in cgNames:
+            tasks_cg.append(getConsumerGroupOffsets(topicName, cgName))
+            # cgOffsetsInfo = getConsumerGroupOffsets(topicName, cgName)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(asyncio.gather(*tasks_cg))
+    loop.close()
+
+    return "ok"
+
+async def getConsumerGroupOffsets(topicName, consumerGroupName):
+    '''
+    returns the consumer group offset info in the form of a dictionary: {partition_id: offset)
+    '''
+    topic = client.topics[topicName]
+    consumer = topic.get_simple_consumer(consumer_group=consumerGroupName,
+                                         auto_start=False,
+                                         reset_offset_on_fetch=False)
+    current_offsets = consumer.fetch_offsets()
+    # filter out consumer group offsets of '-1'
+    consumerGroupOffsetInfo = {}
+
+    for p_id, res in current_offsets:
+        if res.offset >= 0:
+            consumerGroupOffsetInfo[p_id] = res.offset
+
+    for partition_id, offset in consumerGroupOffsetInfo.items():
+        consumerGroupOffsetTracker.labels('broker', topicName, partition_id, consumerGroupName).set(offset)
+
+    cgOffsetsInfo_list.append(consumerGroupOffsetInfo)
+
+def getOffsetInfo(topicName=None):
+    '''
+    get offset info for the given topic. If no topic is given, returns offset info for all topics
+    '''
+
+    # default is to get offset info for all topics
+    # print(client.topics.keys())
+    topic_names = client.topics.keys()
+    #    loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # loop = asyncio.get_event_loop()
+
+    if topicName:
+        if topicName not in topic_names:
+            LOGGER.error("Topic '{}' does not exist".format(topicName))
+            raise ValueError("topic {} doesn't exist!".format(topicName))
+        else:
+            topic_names = [topicName]
+
+    tasks = []
+
+    for topic_name in topic_names:
+        tasks.append(getOffset(topic_name))
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(asyncio.gather(*tasks))
+    loop.close()
+
+    return offset_info
+
+async def getOffset(topic_name):
+    try:
+        earliest_offsets = client.topics[topic_name].earliest_available_offsets()
+        latest_offsets = client.topics[topic_name].latest_available_offsets()
+        offset_info[str(topic_name, 'utf-8')] = {'earliest': earliest_offsets, 'latest': latest_offsets}
+    except Exception:
+        pass
+
+def getLatestOffset(topicName=None):
+    offset_info = getOffsetInfo(topicName)
+    if not offset_info:
+        return None
+
+    # return the (topic, partition, offset) tuple
+    latestOffsets = []
+    for topicName in offset_info.keys():
+
+        latestOffsetInfo = offset_info[topicName]['latest']
+        for partition in latestOffsetInfo:
+            offsetPartitionResponse = latestOffsetInfo[partition]
+            # LOGGER.info(offsetPartitionResponse)
+            latestOffsets.append(
+                {'topic': topicName, 'partition': partition, 'offset': offsetPartitionResponse.offset[0]})
+
+    return latestOffsets
 
 
 def main():
